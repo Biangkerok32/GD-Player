a7d9a17ac7c65d61906ef1712845da84161c19c5
cott331930 <67470867+cott331930@users.noreply.github.com>
2020-06-26 23:10:39 +0800

    GD!
    first commit

---
+++ GDplayList/playlist.js
@@ -0,0 +1,19 @@
+// YT-video id
+let playList = [
+    "kiFvymj0i0Y", // not revival(Zilean not ult
+    "jmrSmDUOwT8", // WTF u doing?
+    "OAfpNG2Cd2E", // Inting never stop
+    "5qeY9f0hrxc", // WTF Flash 
+    "ZJJC7ia51y8", // How dare u come down here?AHHAAAAAA! Suck!
+    "ZJJC7ia51y8"  // Joneeeee
+
+];
+
+let playTime=[
+    [26,34],
+    [43,47],
+    [9,16],
+    [8.5,12],
+    [58.3,74],
+    [98,101]
+]
